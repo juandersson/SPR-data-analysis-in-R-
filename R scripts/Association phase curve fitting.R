fcd7bfbcde1f773079efe40a4a115073ef3c804d
juandersson <103636512+juandersson@users.noreply.github.com>
2022-05-27 10:13:29 +0100

    Update Association phase curve fitting.R

--- R scripts/Association phase curve fitting.R
+++ R scripts/Association phase curve fitting.R
@@ -6,31 +6,31 @@
 #                                          ~Monophasic model~
 #------------------------------------------------------------------------------------------------------------------
 # Note that the following code is set up to model the simulated data "monoassim" and certain code will need to be 
-# specified according to your data.  
+# specified according to the data being analysed.
 
 #Create vectors for the response and the time. 
 RU <- monoassim$RU   
 t <- monoassim$Time                      
       #In the simulated data these columns are labeled "RU" and "Time", but this will need to be specified 
-      #according to your data. 
+      #according to the data being analysed. 
 
 #Plot the data 
 plot(t, RU, yaxs = "i", xaxs = "i", xlab = "Time(s)", ylab = "RU",xlim = c(0,210), ylim = c(0,155)) 
-      #the "xlim" and "ylim" functions are used to set the range of the x and y-axes of the graph.
-      #choose which values are most suitable to visualize your curve 
+      #The "xlim" and "ylim" functions are used to set the range of the x and y-axes of the graph.
+      #values suitable to visualise the the data being analysed should be specified. 
 
 #Create a fit for the curve using the non-linear regression function with a monophasic model 
 fit <- nls(RU ~ (Beq*(1-exp(-t*kobs))), start=list (Beq = 160, kobs = 0.1)) 
       #Starting values, or estimations, must be set for the parameters in the command "start=list()".
-      #The values set must be within a reasonable range for the potential parameter values. 
-      #Otherwise the model algorithm will not be able to converge and you will receive an error message. 
-      #These estimations are naturally specific to your data. 
+      #The values set must be within a reasonable range for the potential parameter values, 
+      #otherwise the model algorithm will not be able to converge and an error message will be received. 
+      #These estimations are naturally specific to the data being analysed. 
 
-#Create a data frame of the values of "t" for which you want to get predictions of the fit.   
+#Create a data frame of the values of "t" for which to obtain predictions of the fit.   
 new.data <- data.frame(t = seq(min(t),max(t),len = 200)) 
-      #Adjust "len =" according to the association time of your data                               
+      #Adjust "len =" according to the association time of the data being analysed.                                
                               
-#Visualise the modeled fit in your plot.
+#Visualise the modeled fit in the plot.
 lines(new.data$t,predict(fit,newdata = new.data), col = "red", lwd = "2")
 
 #View the estimated values of the parameters.
@@ -40,17 +40,19 @@ summary(fit)
 #                                          ~Biphasic model~
 #------------------------------------------------------------------------------------------------------------------
 # Note that the following code is set up to model the simulated data "biassim" and certain code will need to be 
-# specified according to your data.
+# specified according to the data being analysed.   
 
-#similarly to the code using a monophasic model, run the following code with specifications according to your data: 
+#Similarly to the code using a monophasic model, run the following code with specifications according to the data 
+#being analysed: 
 RU <- biassim$RU
 t <- biassim$Time
 plot(t, RU, yaxs = "i", xaxs = "i", xlim = c(0,210), ylim = c(0,340), xlab = "Time(s)", ylab = "RU")
 
 #Create a fit for the curve using the non-linear regression function with a biphasic model 
-fit <- nls(RU ~ (Beq1*(1-exp(-t*kobs1))) + (Beq2*(1-exp(-t*kobs2))), start = list (Beq1 = 200, kobs1 = 0.1, Beq2 = 100, kobs2 = 0.01))
+fit <- nls(RU ~ (Beq1*(1-exp(-t*kobs1))) + (Beq2*(1-exp(-t*kobs2))), 
+           start = list (Beq1 = 200, kobs1 = 0.1, Beq2 = 100, kobs2 = 0.01))
 
-#Visualise your fit, and get the estimated values for the parameters.  
+#Visualise the fit, and get the estimated values for the parameters.  
 new.data <- data.frame(t = seq(min(t),max(t),len = 200))
 lines(new.data$t,predict(fit,newdata = new.data), col = "red", lwd =  "2")
 summary(fit)              
