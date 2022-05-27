1af41c21885b87ce5f551e5507b647135d8235ea
juandersson <103636512+juandersson@users.noreply.github.com>
2022-05-27 17:06:28 +0100

    Update Equilibrium analysis.R

--- R scripts/Equilibrium analysis.R
+++ R scripts/Equilibrium analysis.R
@@ -4,7 +4,7 @@
 # This R script provides code for fitting a non linear model to the relationship between Beq and analyte concentration
 # in order to derive the KD of a reaction. 
 
-# Create vectrors for the concentration variable and corresponding Beq values.The first value should be 0 
+# Create vectors for the concentration variable and corresponding Beq values. The first value should be 0 
 # for both variables 
 # The following values are simply examples
 c <- c(0,4,8,16,31,62,125,250,500)
@@ -13,7 +13,7 @@ Beq <- c(0, 13, 32, 55, 88, 145, 202, 245, 280)
 #Plot the data 
 plot(c,Beq, ylim = c(0,300),xlab="[analyte]", ylab="Beq")
       #the "xlim" is set to best visualise the example data
-      #choose which range of X-values are most suitable to visualize the data being analysed.   
+      #choose which range of X-values are most suitable to visualise the data being analysed.   
 
 #Create a fit for the curve using the non-linear regression function 
 fit <- nls(Beq ~ Bmax * (c/(KD +c)), start = list(Bmax = 350 , KD = 150))
@@ -22,10 +22,9 @@ fit <- nls(Beq ~ Bmax * (c/(KD +c)), start = list(Bmax = 350 , KD = 150))
       #Otherwise the model algorithm will not be able to converge and an error message will be received. 
       #These estimations are naturally specific to the data being analysed.   
 
-
 #Create a data frame of the values of "c" for which to get predictions of the fit.   
 new.data <- data.frame(c = seq(min(c),max(c),len = 500)) 
-      #Adjust "len =" according to the highest analyte concentration of the data being analysed.                              
+      #Adjust "len" according to the highest analyte concentration of the data being analysed.                              
 
 
 #Visualise the modeled fit in the plot.
