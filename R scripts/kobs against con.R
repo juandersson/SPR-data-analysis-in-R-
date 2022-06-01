118198ab15087d71f82086e1d494925871335a90
juandersson <103636512+juandersson@users.noreply.github.com>
2022-06-01 14:18:21 +0100

    Update and rename Kobs against con.R to kobs against con.R

--- R scripts/kobs against con.R
+++ R scripts/kobs against con.R
@@ -1,21 +1,21 @@
 #------------------------------------------------------------------------------------------------------------------
-#                                    ~~ Deriving KD from Kobs values ~~
+#                                    ~~ Deriving KD from kobs values ~~
 #------------------------------------------------------------------------------------------------------------------
-# This R script provides code for fitting a linear model for the relationship between Kobs and analyte concentration
+# This R script provides code for fitting a linear model for the relationship between kobs and analyte concentration
 # in order to derive the KD of a reaction. 
 
-# Create vectors for the concentration variable and corresponding Kobs values.
+# Create vectors for the concentration variable and corresponding kobs values.
 # The following values are simply examples. 
 c <- c(4,8,16,31,62,125,250,500)
-Kobs <- c(0.0039604, 0.0040308, 0.0042716, 0.0050106, 0.0061333, 0.008715, 0.01414, 0.0247)
+kobs <- c(0.0039604, 0.0040308, 0.0042716, 0.0050106, 0.0061333, 0.008715, 0.01414, 0.0247)
 
 #Plot the data 
-plot(c,Kobs, ylim = c(0,0.03),xlab="[analyte]", ylab="Kobs")
+plot(c,kobs, ylim = c(0,0.03),xlab="[analyte]", ylab="kobs")
       #the "xlim" is set to best visualise the example data
       #choose which range of X-values are most suitable to visualise the data being analysed.   
 
 #Create a linear fit for the curve using the "lm" function 
-fit <- lm(Kobs ~ c)
+fit <- lm(kobs ~ c)
 
 #visualise the fit in the plot 
 abline(fit, col = "red")
